8016fe1da1a3d5c727f022d1575ac1624d38643c
Richard Ailton Alfaro Manzano <richardalfaromanznao@gmail.com>
2023-01-14 22:09:50 -0500

    'f'

---
+++ assets/js/index.js
@@ -0,0 +1,34 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.me' , {
+    duration: 2000,
+    origin:'right',
+    delay:100,
+    distance: '10px'
+})
+sr.reveal('.about__cards' , {
+    duration: 3000,
+    origin:'left',
+    distance: '100px'
+})
+sr.reveal('.animado' , {
+    duration: 2000,
+    delay: 900,
+    origin:'right',
+    distance: '20px'
+})
+sr.reveal('.odd' , {
+    duration: 2000,
+    origin:'left',
+    distance: '50px'
+})
+sr.reveal('.even' , {
+    duration: 3000,
+    origin:'right',
+    distance: '30px'
+})
+sr.reveal('div.studies__grid' , {
+    duration: 2000,
+    origin:'right',
+    distance: '10px'
+})
